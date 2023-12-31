0452a5f4bab211d9a2b258dfdb3e485e8940593b
Bartosz Woldanski <studio@dziwnykot.pl>
2023-12-31 02:52:07 +0100

    feat: add dynamic string for file name
    Add POST body element that manage dynamic part of saved PNG file name.

--- public/php/save-png.php
+++ public/php/save-png.php
@@ -2,13 +2,14 @@
 
 $_POST = json_decode(file_get_contents("php://input"), true);
 $img = $_POST['img'];
+$name = $_POST['fileName'];
 
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 
 define('ROOTPATH', __DIR__ . "/img/");
 
-$file = "./". time() . '.png';
+$file = "./".$name.'.png';
 
 file_put_contents($file, base64_decode($img));
 
