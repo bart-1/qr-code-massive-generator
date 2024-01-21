04e036f9585baf778ab4bbcc717cc6aef3eb87bc
Bartosz Woldanski <studio@dziwnykot.pl>
2024-01-21 17:28:12 +0100

    fix: modify function changeMultiArrayToObjectsArray (old name: arraysArrayToObject ): - better typing - avoid reference problem with all objects in array refactor: better functions names refactor: change type to interface

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,31 +1,66 @@
-export const fileDataStringToArraysArray = (data: string | ArrayBuffer): Array<string[]> => {
+interface Property {
+  [key: string]: string;
+}
+export interface FileCSVData extends Property {
+  lastname: string;
+  firstname: string;
+  organization: string;
+  jobTitle: string;
+  street: string;
+  city: string;
+  state: string;
+  postCode: string;
+  country: string;
+  workPhone: string;
+  cellPhone: string;
+  fax: string;
+  email: string;
+  url: string;
+}
 
+export const initialFileCSVData: FileCSVData = {
+  lastname: "",
+  firstname: "",
+  organization: "",
+  jobTitle: "",
+  street: "",
+  city: "",
+  state: "",
+  postCode: "",
+  country: "",
+  workPhone: "",
+  cellPhone: "",
+  fax: "",
+  email: "",
+  url: "",
+};
+
+export const convertDataStringToMultiArray = (
+  data: string | ArrayBuffer
+): Array<string[]> => {
   const firstStep = String(data).replaceAll("\t", "|").split("\n");
   const secondStep = firstStep.map((el) => el.split("|"));
 
   return secondStep;
 };
 
+export const changeMultiArrayToObjectsArray = (array: Array<string[]>): FileCSVData[] => {
+  const keysArray = array[0];
+  const  objectsArray: FileCSVData[] = [initialFileCSVData];
+  let singleObject: FileCSVData = initialFileCSVData;
 
-export const arraysArrayToObject = (array:Array<string[]>):{}[] => {
+  array.map((arr) => {
+    keysArray.forEach((element, index) => {
+      if (singleObject.hasOwnProperty(element))
+        singleObject[element] = arr[index];
+    });
+    objectsArray.push({ ...singleObject }); //spread operator is added for making copy of object, without it every iteration add to array only reference of object, so after this if it changes property value in one object, it changes by reference in each object of  array.
+  });
 
-    const keys = array[0];
-    let objects: {}[] = [];
-    
-    for (let i = 1; i < array.length-1; i++) {
-        let record = {};
-            array[i].map((el, index) => 
-            record = {...record, [keys[index]]: el }
-              );
-        objects = [...objects, record];
-}
-    
-    return objects;
-}
-
+  return objectsArray;
+};
 
 export const saveToPng = (img: string, fileName: string | number) => {
-
   fetch("http://127.0.0.1/save-png.php", {
     method: "POST",
     mode: "no-cors",
@@ -39,27 +74,8 @@ export const saveToPng = (img: string, fileName: string | number) => {
   });
 };
 
-
-export type FileCSVData = {
-    lastname: string;
-    firstname: string;
-    organization: string;
-    jobTitle: string;
-    street: string;
-    city: string;
-    state: string;
-    postCode: string;
-    country: string;
-    workPhone: string;
-    cellPhone: string;
-    fax: string;
-    email: string;
-    url: string;
-
-
-}
-
-export const vCard = (vCardForm:FileCSVData):string =>{  return `BEGIN:VCARD
+export const createVCardString = (vCardForm: FileCSVData): string => {
+  return `BEGIN:VCARD
 VERSION:3.0
 N:${vCardForm.lastname};${vCardForm.firstname}
 FN:${vCardForm.firstname} ${vCardForm.lastname}
@@ -72,4 +88,4 @@ TEL;FAX:${vCardForm.fax}
 EMAIL;WORK;INTERNET:${vCardForm.email}
 URL:${vCardForm.url}
 END:VCARD`;
-}
+};
