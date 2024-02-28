fb18c303905b52d7b6ad804974696b4ae6bb69e2
Bartosz Woldanski <studio@dziwnykot.pl>
2024-02-28 18:21:05 +0100

    Modify/fix save-png.php
    It handles new varables in POST request body for
    better tracking folders on server.
    
    Added code for creation of DIR if needed.
    
    It fixes paths tracking.

--- public/php/save-png.php
+++ public/php/save-png.php
@@ -1,14 +1,19 @@
 <?php
 
-$_POST = json_decode(file_get_contents("php://input"), true);
-$img   = $_POST['img'];
-$name  = $_POST['fileName'];
+$_POST      = json_decode(file_get_contents("php://input"), true);
+$img        = $_POST['img'];
+$name       = $_POST['fileName'];
+$folderName = $_POST['folderName'];
+
+$path = '../img/' . $folderName;
+
+if (!file_exists($path)) {
+ mkdir($path, 0777, true);
+}
 
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 
-define('ROOTPATH', __DIR__ . "/img");
-
-$file = "./img/" . $name . '.png';
+$file = $path. '/' . $name . '.png';
 
 file_put_contents($file, base64_decode($img));
